c2e7abb8f3f63aa9d1c781c164915d83d67b599a
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2018-11-13 01:02:05 +0530

    Automatic push

---
+++ ICAN.SIC.Abstractions/IMessageVariants/IMachineImageMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ICAN.SIC.Abstractions.IMessageVariants
+{
+    public interface IMachineImageMessage : IMessage
+    {
+        string FileName { get; }
+        Image Image { get; }
+    }
+}
