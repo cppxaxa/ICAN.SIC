1eec80ff14457f77f80bc38c80e5cf3298d57721
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2018-11-19 22:31:42 +0530

    Automatic push

---
+++ ICAN.SIC.Abstractions/IMessageVariants/ICANSEE.IInformationMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICAN.SIC.Abstractions.IMessageVariants.ICANSEE
+{
+    public interface IInformationMessage : IMessage
+    {
+        string Json { get; }
+        string Text { get; }
+    }
+}
---
+++ ICAN.SIC.Abstractions/IMessageVariants/ICANSEE.IInputMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICAN.SIC.Abstractions.IMessageVariants.ICANSEE
+{
+    public enum ControlFunction
+    {
+        ExecutePreset,
+        ListAllCameraConfigurations,
+        ListAllComputeDevices,
+        QueryComputeDevice,
+        ListAllCameraInUse,
+        UnloadPreset,
+        UnloadCamera,
+        UnloadAlgorithm,
+        AddCamera,
+        LoadCamera,
+        LoadAlgorithm,
+        LoadDeviceLocalImage,
+        DisplayImageInServerGUI,
+        RequestImageMessage
+    }
+
+    public interface IInputMessage : IMessage
+    {
+        ControlFunction ControlFunction { get; }
+        List<string> Parameters { get; }
+
+        string Text { get; }
+    }
+}
